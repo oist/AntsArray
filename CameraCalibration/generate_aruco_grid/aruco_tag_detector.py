2b44b729dcd60ebd574baa6f48aacdd865eb4874
Makoto Hiroi <makotohiroi@gmail.com>
2024-03-28 14:58:02 +0900

    modify ruco.DetectorParameters to Sam's one

--- CameraCalibration/generate_aruco_grid/aruco_tag_detector.py
+++ CameraCalibration/generate_aruco_grid/aruco_tag_detector.py
@@ -1,6 +1,7 @@
 import cv2
 import cv2.aruco as aruco
 
+
 def detect_aruco_tags(image_path):
     # Load the image
     img = cv2.imread(image_path)
@@ -10,16 +11,18 @@ def detect_aruco_tags(image_path):
 
     # Get the ArUco dictionary
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_1000)
-    
+
     # Create default parameters for detection
     parameters = aruco.DetectorParameters()
 
     # Adjust detection parameters
-    parameters.adaptiveThreshConstant = 8  # Default is 7, adjust based on lighting conditions
-    parameters.minMarkerPerimeterRate = 0.01  # Default is 0.03, adjust for marker size relative to image
-    parameters.maxMarkerPerimeterRate = 4.0  # Default is 4.0, adjust to avoid detecting very large markers
-    parameters.polygonalApproxAccuracyRate = 0.1  # Default is 0.05, lower values can make detection more precise
-    
+    # Default is 7, adjust based on lighting conditions
+    parameters.adaptiveThreshConstant = 3
+    parameters.adaptiveThreshWinSizeMin = 10
+    parameters.adaptiveThreshWinSizeMax = 40
+    parameters.adaptiveThreshWinSizeStep = 10
+    parameters.errorCorrectionRate = 1
+
     # Create an instance of ArucoDetector with custom parameters
     detector = aruco.ArucoDetector(aruco_dict, parameters)
 
@@ -32,12 +35,15 @@ def detect_aruco_tags(image_path):
         aruco.drawDetectedMarkers(img, corners, ids)
 
     # Resize for display if necessary
-    resized_img = cv2.resize(img, (1600, int(1600 * (img.shape[0] / img.shape[1]))))
-    
+    resized_img = cv2.resize(
+        img, (1600, int(1600 * (img.shape[0] / img.shape[1]))))
+
     # Display the output image
     cv2.imshow('Detected ArUco tags', resized_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 # Replace the path with the actual path to your image file
-detect_aruco_tags(r"Z:\ReiterU\Ants\basler\QRcodes_test\f4_gain15_Image__2024-02-29__18-22-55.tiff")
+detect_aruco_tags(
+    r"Z:\ReiterU\Ants\basler\Anouk\Image__2024-03-22__17-32-08.tiff")
